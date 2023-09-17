9f7f6f4541336ab7efc5a5bfa9e053d5a3874818
NeelanjanaGoswami <87737175+NeelanjanaGoswami@users.noreply.github.com>
2023-09-17 14:50:46 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 music1="";
 music2="";
 
+
 function preload(){
     music1 = loadSound("music.mp3");
     music2 = loadSound("music1.mp3");
 }
 
-scoreRightWrist = 0;
-scoreLeftWrist = 0;
 
 rightWristX= 0;
 rightWristY= 0;
@@ -26,10 +25,32 @@ function setup(){
     poseNet.on('pose', gotPoses);
 }
 
+function gotPoses(results){
+    if(results.length > 0){
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " +  leftWristX + "leftWristY = " + leftWristY);
+    }
+
+}
+
 function modelLoaded(){
     console.log('PoseNet Is Initialized');
 }
 
 function draw(){
     image(video, 0, 0, 600, 500);
+}
+
+function play(){
+    music1.play();
+    music1.setVolume(1);
+    music1.rate(1);
+    music2.play();
+    music2.setVolume(1);
+    music2.rate(1);
 }
